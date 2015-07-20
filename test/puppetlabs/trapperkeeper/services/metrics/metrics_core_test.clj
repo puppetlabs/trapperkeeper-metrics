de80c41c5de79873e7666c0484d622e9673ce2db
Richard Clamp <richardc@unixbeard.net>
2015-07-20 12:19:35 +0100

    TK-253 become a wrapper for metrics-clojure's default-registry
    Here we depend on the metrics-clojure wrapper for Dropwizard Metrics and use
    the global MetricsRegistry that it creates rather than construct our own.
    
    This allows a hybrid usage pattern where metrics may be declared globally and
    used without threading the tk-metrics MetricsRegistry though the application,
    but we are still able to configure JMX Publishing via service configuration files.

--- test/puppetlabs/trapperkeeper/services/metrics/metrics_core_test.clj
+++ test/puppetlabs/trapperkeeper/services/metrics/metrics_core_test.clj
@@ -1,6 +1,7 @@
 (ns puppetlabs.trapperkeeper.services.metrics.metrics-core-test
   (:import (com.codahale.metrics MetricRegistry JmxReporter))
   (:require [clojure.test :refer :all]
+            [metrics.core]
             [puppetlabs.trapperkeeper.services.metrics.metrics-core :refer :all]
             [schema.test :as schema-test]))
 
@@ -10,9 +11,10 @@
   (testing "does not initialize registry if metrics are disabled"
     (let [context (initialize {:enabled false :server-id "localhost"})]
       (is (nil? (:registry context)))))
-  (testing "initializes registry and adds to context if metrics are enabled"
+  (testing "adds the metrics.core/default-registry to context"
     (let [context (initialize {:enabled true :server-id "localhost"})]
       (is (instance? MetricRegistry (:registry context)))
+      (is (= metrics.core/default-registry (:registry context)))
       (is (not (contains? context :jmx-reporter)))))
   (testing "enables jmx reporter if configured to do so"
     (let [context (initialize {:enabled true
