fc3ced241fe625f2fec5789bfb8e2121e17c6373
Jenkins CI <ci@puppet.com>
2020-03-31 18:41:19 +0000

    Version 1.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-metrics "1.2.2-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-metrics "1.2.2"
   :description "Trapperkeeper Metrics Service"
   :url "http://github.com/puppetlabs/trapperkeeper-metrics"
 
