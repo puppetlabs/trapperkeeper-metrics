81a610863d155699378cd5e1b1ea2f9b69aefd40
Austin Blatt <austin.blatt@puppet.com>
2021-01-26 14:09:58 -0800

    (maint) Prep for 1.4.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-metrics "1.3.2-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-metrics "1.4.0-SNAPSHOT"
   :description "Trapperkeeper Metrics Service"
   :url "http://github.com/puppetlabs/trapperkeeper-metrics"
 
